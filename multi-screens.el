a2b1f69d75a04f6a8860c9e90345bd66da5a2102
stardiviner <numbchild@gmail.com>
2020-07-01 10:52:32 +0800

    create empty source code file

---
+++ multi-screens.el
@@ -0,0 +1,40 @@
+;;; multi-screens.el --- Minor mode to controlling frames for multiple screens -*- lexical-binding: t; -*-
+
+;;; Time-stamp: <2020-07-01 10:52:22 stardiviner>
+
+;; Authors: stardiviner <numbchild@gmail.com>
+;; Package-Requires: ((emacs "25"))
+;; Package-Version: 0.1
+;; Keywords: frames
+;; homepage: https://github.com/stardiviner/multi-screens.el
+
+;; multi-screens is free software; you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; multi-screens is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+;; or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+;; License for more details.
+;;
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; Usage:
+;; (add-hook 'after-init-hook #'multi-screens-mode)
+
+;;; Code:
+
+
+
+
+
+
+
+(provide 'multi-screens)
+
+;;; multi-screens.el ends here
